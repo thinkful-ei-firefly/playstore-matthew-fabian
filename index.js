5c4c8180c1992de96de8c56443a02eeaab327854
Matthew Mbonu <mcmbonu1@gmail.com>
2019-08-20 11:47:37 -0400

    finished app

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-const express = require('express');
-const playStoreData = require('./playstore');
+const express = require("express");
+const playStoreData = require("./playstore");
 
 const app = express();
 
-app.get('/apps', (req, res) =>{
-  let {sort, genres} = req.query;
-  const validSort = ['rating', 'app'];
-  const validGenre = ['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'];
+app.get("/apps", (req, res) => {
+  let { sort, genres } = req.query;
 
-  if (!validSort.includes(sort.toLowerCase())){
-    return res.status(400).json({error: "Sort should be one of: " + validSort.join(', ')});
+  const validSort = ["Rating", "App"];
+  const validGenre = [
+    "action",
+    "puzzle",
+    "strategy",
+    "casual",
+    "arcade",
+    "card"
+  ];
+
+  if (sort) {
+    if (!validSort.includes(sort)) {
+      return res
+        .status(400)
+        .json({ error: "Sort should be one of: " + validSort.join(", ") });
+    }
   }
-  if (!validGenre.includes(genres.toLowerCase())){
-    return res.status(400).json({error: "Genres should be one of: " + validGenre.join(', ')});
+
+  if (genres) {
+    if (!validGenre.includes(genres.toLowerCase())) {
+      return res
+        .status(400)
+        .json({ error: "Genres should be one of: " + validGenre.join(", ") });
+    }
   }
 
   let filteredData = [...playStoreData];
 
-  if (genres){
-    filteredData = filteredData.filter(data => data.Genres.toLowerCase().includes(genres.toLowerCase()));
+  if (genres) {
+    filteredData = filteredData.filter(data =>
+      data.Genres.toLowerCase().includes(genres.toLowerCase())
+    );
+  }
+  if (sort) {
+    filteredData = filteredData.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
   }
 
   res.json(filteredData);
-})
+});
 
-app.listen(8080, () => console.log('Server running..'));
+app.listen(8080, () => console.log("Server running.."));
